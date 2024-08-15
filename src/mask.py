7acdcc268d1856a6364f603ff419884e274f1929
stanislav <stasfed16@gmail.com>
2024-08-15 16:05:34 +0500

    rework homework

--- src/mask.py
+++ src/mask.py
@@ -4,3 +4,11 @@ def get_mask_card_number(card_num: int) -> str | None:
         return f"{card_num[:5]} {card_num[4: 6]}{"*" * 2} {"*" * 4} {card_num[12:]}"
     else:
         return None
+
+
+def get_mask_account(num_acc: str) -> str | None:
+    """Функция маскировки номера счета"""
+    if num_acc.isdigit() and len(num_acc) == 20:
+        return f"** {num_acc[-4:]}"
+    else:
+        return None
--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,16 @@
-from src.mask import mask_acc_num, mask_card_num
+from src.mask import get_mask_card_number, get_mask_account
+
 
 def mask_account_card(num_acc_card: str) -> str | None:
     """Функция маскировки счета и карты"""
     if len(num_acc_card.split()[-1]) == 16:
-        new_num = mask_card_num(num_acc_card.split()[-1])
+        new_num = get_mask_card_number(num_acc_card.split()[-1])
         result = f"{num_acc_card[:-16]}{new_num}"
         return result
     elif len(num_acc_card.split()[-1]) == 20:
-        new_num_acc = mask_acc_num(num_acc_card.split()[-1])
+        new_num_acc = get_mask_account(num_acc_card.split()[-1])
         results = f"{num_acc_card[:-20]}{new_num_acc}"
         return results
-    else:
-        return None
 
 
 
