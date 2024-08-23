ce21cea44cf3b034c7a0db6d85a787b4fda8d691
stanislav <stasfed16@gmail.com>
2024-08-23 23:10:35 +0500

    fix 3

--- src/mask.py
+++ src/mask.py
@@ -9,6 +9,6 @@ def get_mask_card_number(card_num: int) -> str | None:
 def get_mask_account(num_acc: str) -> str | None:
     """Функция маскировки номера счета"""
     if num_acc.isdigit() and len(num_acc) == 20:
-        return f"** {num_acc[-4:]}"
+        return f"**{num_acc[-4:]}"
     else:
         return None
