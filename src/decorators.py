fb9b4ba6da4deefb00c355ec36722b678dfc34cc
stanislav <stasfed16@gmail.com>
2024-08-27 10:00:55 +0500

    add decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,32 @@
+from functools import wraps
+
+
+def log(filename):
+    """ декоратор для записи вызова функции в консоль или в mylog"""
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"my_function ok\n")
+                else:
+                    print(f"my_function ok\n")
+            except Exception as e:
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"my_function error: {e}. Inputs {args}, {kwargs}\n")
+                else:
+                    print(f"my_function error: {e}. Inputs {args}, {kwargs}")
+            return result
+        return wrapper
+    return decorator
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    """Функция складывания двух чисел"""
+    return x + y
+
+my_function(1, 2)
--- src/mask.py
+++ src/mask.py
@@ -1,4 +1,4 @@
-def get_mask_card_number(card_num: int) -> str | None:
+def get_mask_card_number(card_num: str) -> str | None:
     """Функция маскировки номера карты"""
     if card_num.isdigit() and len(card_num) == 16:
         return f"{card_num[:4]} {card_num[4: 6]}{"*" * 2} {"*" * 4} {card_num[12:]}"
