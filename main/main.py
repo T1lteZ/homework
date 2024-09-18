e7f3262892ff92883b46525ed289654178f5b10b
stanislav <stasfed16@gmail.com>
2024-09-18 05:13:10 +0500

    fix

--- main/main.py
+++ main/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.append(os.getcwd())
 from src.widget import get_date, mask_account_card
 from src.bank_operations import search_bank_operation
 from src.generators import filter_by_currency
@@ -7,7 +8,6 @@ from src.processing import filter_by_state, sort_by_date
 from src.financy import csv_reader_func, xlsx_reader_func
 from src.utils import operation_list
 
-sys.path.append(os.getcwd())
 
 PATH_TO_FILE_JSON = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.json")
 PATH_TO_FILE_CSV = "C:/Users/stasf/PycharmProjects/homework/financy.csv"
@@ -45,8 +45,14 @@ def main_menu():
 
         if operation_status not in status_list:
             print(f"Статус операции {operation_status} недоступен.")
+            operation_status = input("Попробуйте снова")
+            if operation_status in status_list:
+                print(f"Выбор {operation_status}")
+            else:
+                break
         else:
             print(f"Выбор {operation_status}")
+            break
 
         filtered_transactions = filter_by_state(operations, state=operation_status)
 
@@ -57,17 +63,22 @@ def main_menu():
                 date_flag = False
             else:
                 date_flag = True
+        elif date_sorted == "нет":
+            continue
+        else:
+            continue
         filtered_transactions = sort_by_date(filtered_transactions, date_flag)
 
-        filter_by_rub = input("Выводить только рублевые транзакции? Да/Нет ").lower
-        if filter_by_rub == "да":
-            rub_transactions = filter_by_currency(filtered_transactions, currency="RUB")
-            filtered_transactions = list(rub_transactions)
-        else:
-            print(" ")
+        next_choice_rub = """Выводить только рублевые тразакции? Да/Нет"""
+        input_user_rub = input(f"{next_choice_rub}\n").lower()
+        while input_user_rub not in ["да", "нет"]:
+            print("\nВвели некорректную сортировку\nПопробуйте еще раз:")
+            input_user_rub = input(f"{next_choice_rub}\n").lower()
+            if input_user_rub == "да":
+                filtered_transactions = filter_by_currency(filtered_transactions, "RUB")
 
-        fitered_by_word = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет").lower
-        if fitered_by_word == "да":
+        filtered_by_word = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет").lower
+        if filtered_by_word == "да":
             word = input("Введите слово...")
             filtered_transactions = search_bank_operation(filtered_transactions, word)
         else:
@@ -87,7 +98,7 @@ def main_menu():
                 date = get_date(transaction.get("date"))
 
                 amount = transaction["operationAmount"]["amount"]
-                currency = transaction["operationAmount"]["currency"]["name"]
+                currency = transaction["operationAmount"]["currency"]["code"]
 
                 if description == "Открытие вклада":
                     print(f"{date} {description}\nСчет {to_}\nСумма: {amount} {currency}\n")
@@ -99,16 +110,20 @@ def main_menu():
                 if description == "Открытие вклада":
                     from_ = description
                 else:
-                    from_ = (transaction.get("from"))
+                    from_ = mask_account_card(transaction.get("from"))
 
                 date = get_date(transaction.get("date"))
-                to_ = transaction.get("to")
+
 
                 amount = transaction.get("amount")
                 currency = transaction.get("currency_name")
 
                 if description == "Открытие вклада":
+                    to_ = mask_account_card(transaction.get("to"))
                     print(f"{date} {description}\nСчет {to_}\nСумма: {amount} {currency}\n")
                 else:
+                    to_ = mask_account_card(transaction.get("to"))
                     print(f"{date} {description}\n{from_} -> {to_}\nСумма: {amount} {currency}\n")
         break
+
+main_menu()
